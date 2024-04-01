45df257035a92fd69546fcecaaf96bea33e0d31b
LewisBavin <144824398+LewisBavin@users.noreply.github.com>
2024-04-01 17:53:14 +0100

    addedJoi

--- routes/add.js
+++ routes/add.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const sha256 = require("sha256");
+const Joi = require("joi");
+
+const schema = Joi.object.keys({
+  email: Joi.string().email({ tlds: { allow: false } }),
+  username: Joi.string().alphanum().min(4).max(10).required(),
+  password: Joi.string().alphanum().min(8).required(),
+});
 
 router.post("/", (req, res) => {
   let { email, username, password } = req.body;
   let users = req.users;
   let msg = {};
-
+  let result = Joi.valid();
   msg.email = !email
     ? "please enter eMail"
     : !!users.filter((u) => u.email === email)[0]
