1ff30c35974d79ae75adacb30e30d918b07497f8
LewisBavin <144824398+LewisBavin@users.noreply.github.com>
2024-04-30 05:30:42 +0100

    AddedPendingSection

--- routes/get.js
+++ routes/get.js
@@ -16,17 +16,32 @@ router.get("/users", verifyUser, async (req, res) => {
 });
 
 router.get("/requests", verifyUser, async (req, res) => {
-  let userRequests = await promiseSQL(
+  let { start_date, end_date } = req.headers;
+  console.log(start_date, end_date);
+
+  let inputs = await promiseSQL(
     `SELECT *
       FROM requests 
-        WHERE user_id = ${req.body.user_id};`
+        WHERE 
+          (user_id = ${req.body.user_id} AND direction = "B" AND
+            (start_date >= "${start_date}" AND start_date <= "${end_date}"))
+          OR
+          (counter_id = ${req.body.user_id} AND direction = "S" AND
+            (start_date >= "${start_date}" AND start_date <= "${end_date}"))
+    ;`
   );
-  let counterRequests = await promiseSQL(
+  let outputs = await promiseSQL(
     `SELECT *
-      FROM requests 
-           WHERE counter_id = ${req.body.user_id};`
+    FROM requests 
+      WHERE 
+        (user_id = ${req.body.user_id} AND direction = "S" AND
+          (start_date >= "${start_date}" AND start_date <= "${end_date}"))
+        OR
+        (counter_id = ${req.body.user_id} AND direction = "B" AND
+          (start_date >= "${start_date}" AND start_date <= "${end_date}"))
+  ;`
   );
-  res.send({ status: 1, userRequests, counterRequests });
+  res.send({ status: 1, inputs, outputs });
 });
 
 router.get("/:id/allocations", verifyUser, async (req, res) => {
